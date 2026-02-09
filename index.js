b6768c07459db2c5e0487992ca0f668badabfe73
tangerul123 <tangelm012@gmail.com>
2026-02-09 09:50:47 +0600

    Added REST API CRUD methods (GET, POST, DELETE)

--- index.js
+++ index.js
@@ -1,14 +1,85 @@
 const express = require('express');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const dns = require("node:dns").promises;
+dns.setServers(["1.1.1.1", "8.8.8.8"]);  // Cloudflare + Google DNS
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req , res) => {
+// user : 
+// pass : 
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bv5vaie.mongodb.net/?appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const coffeesCollection = client.db('coffeeDB').collection('coffees')
+
+        app.get('/coffees', async (req, res) => {
+            const cursor = coffeesCollection.find();
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/coffees/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await coffeesCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.post('/coffees', async (req, res) => {
+
+            const newCoffee = req.body
+            console.log(newCoffee);
+
+            const result = await coffeesCollection.insertOne(newCoffee)
+            res.send(result)
+        })
+
+        app.delete('/coffees/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeesCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Hello Coffee Store Server');
 });
+
 
 
 app.listen(port, () => {
